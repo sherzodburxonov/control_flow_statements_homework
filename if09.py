291b351b3f501cd380a40f7d204622f93bbc080f
burxonovsherzod2105@gmail.com <burxonovsherzod2105@gmail.com>
2023-04-16 23:09:21 +0500

    09

--- if09.py
+++ if09.py
@@ -9,4 +9,13 @@ def main(a):
     Returns:
         boolean: True if the resulting number is less than or equal to the old number, otherwise return False.
     """
-    return
+    first=a//10
+    second=a%10
+    b=second*10+first
+    if a>=b:
+        c="True"
+    if a<b:
+        c="false"
+    return c
+a=int(input("a="))
+print(main(a))
